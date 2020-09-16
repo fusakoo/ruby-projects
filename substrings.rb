02172572ba9ae6911975e4ed1c9bf700cb6ed2ec
fusakoo <obatafusako@gmail.com>
2020-09-16 14:57:45 -0700

    Implement substring matching using 'scan' method

--- substrings.rb
+++ substrings.rb
@@ -1,19 +1,16 @@
 # simple method to cross-reference a given string to an array (dictionary)
 
 def substring(string, array)
-  output = {}
+  output = Hash.new(0)
 
-  string.downcase.split(' ').each do |word|
-    if array.include?(word)
-      if output.has_key?(word)
-        output[word] += 1
-      else
-        output[word] = 1
-      end
-    end
+  sample_word = array.select { |word| string.downcase.include? word }
+  sample_word.each do |word|
+    #scan the number of times the word appears in string & check the length of array created
+    output[word] = string.downcase.scan(word).length
   end
   p output
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 substring("below", dictionary)
+substring("Howdy partner, sit down! How's it going?", dictionary)
