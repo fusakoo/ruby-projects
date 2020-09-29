4100ee328000b85a86bf1c5641c8fe027671c68d
fusakoo <obatafusako@gmail.com>
2020-09-29 15:01:36 -0700

    update stock-picker.rb to be partially compliant to style guide

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,15 +1,13 @@
-=begin
-Stock picker
-- Given the array of stock prices, identify the buy and sell date
-- The sell date should come after the buy date
-=end
+# Stock picker
+# - Given the array of stock prices, identify the buy and sell date
+# - The sell date should come after the buy date
 
 def stock_picker(stocks)
   profit = 0
 
   for i in 0...stocks.length
-    # ensures that when looping through the prices, sell date comes after buy date
-    for j in (i+1)...stocks.length do
+    # ensures when looping through the prices, sell date comes after buy date
+    for j in (i + 1)...stocks.length
       if (stocks[j] - stocks[i]) > profit
         profit = stocks[j] - stocks[i]
         buy_date = i
@@ -23,5 +21,5 @@ def stock_picker(stocks)
   p "Total profit would be $#{profit}"
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
-stock_picker([1,3,19,1,18,8,6,10,19])
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+stock_picker([1, 3, 19, 1, 18, 8, 6, 10, 19])
