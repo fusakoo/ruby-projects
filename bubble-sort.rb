d768e3ad4618795eb69f62048f89289e571c20e3
fusakoo <obatafusako@gmail.com>
2020-09-29 15:08:05 -0700

    update bubble-sort.rb to be partially style guide compliant

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,19 +1,16 @@
-=begin
-Bubble sort
-- Takes an array, return sorted array NOT using #sort but bubble sort methodology
-=end
+# Bubble sort
+# - Takes an array, return sorted array NOT using #sort but bubble sorted
 
 def bubble_sort(array)
   n = array.length
-  
-  array.each do |element|
+  array.each do
     # resets 'swapped' in each loop
     swapped = false
 
     # inclusive for loop (that includes high value, n-1)
-    for i in 1..n-1 do
-      if array[i-1] > array[i]
-        array[i-1], array[i] = array[i], array[i-1]
+    for i in 1..(n - 1) do
+      if array[i - 1] > array[i]
+        array[i - 1], array[i] = array[i], array[i - 1]
         swapped = true
       end
     end
@@ -21,7 +18,7 @@ def bubble_sort(array)
   p array
 end
 
-bubble_sort([4,3,78,2,0,2]) #=> [0,2,2,3,4,78]
-bubble_sort([105,3,9,1,0,2,9,200,6,-1])
-bubble_sort(["a","c","dc","b","dba"])
-bubble_sort(["hi","hello","howdy","henlo","ciao"])
+bubble_sort([4, 3, 78, 2, 0, 2]) #=> [0,2,2,3,4,78]
+bubble_sort([105, 3, 9, 1, 0, 2, 9, 200, 6, -1])
+bubble_sort(%w[a c dc b dba])
+bubble_sort(%w[hi hello howdy henlo ciao])
